7c41ac7d171991a07441dbf3db1eca4152eef2d6
Slicer <ahamadabdi878@gmail.com>
2026-09-07 22:32:36 +0300

    Add simple calculator functionality in JavaScript

---
+++ Other Languages/Simple Calculator 5.js
@@ -0,0 +1,19 @@
+const symb = prompt("Symbol? ");
+const fir = Number(prompt("First Number? "));
+const sec = Number(prompt("Second Number? "));
+
+if (symb === '+') {
+console.log("Results:", fir + sec);
+}
+
+if (symb === '-') {
+console.log("Results:", fir - sec);
+}
+
+if (symb === '*') {
+console.log("Results:", fir * sec);
+}
+
+if (symb === '/') {
+console.log("Results:", fir / sec);
+}
